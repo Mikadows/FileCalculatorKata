0e04b373044f126c99ad9f3aee7b1b80daacc6df
Gwendal <gwendou95@hotmail.fr>
2021-01-18 17:27:50 +0100

    A bit trash less

--- src/main/java/FileCalculatorKata.java
+++ src/main/java/FileCalculatorKata.java
@@ -16,21 +16,33 @@ public class FileCalculatorKata {
 
         List<String> lines = Files.readAllLines(Paths.get(inputFile), StandardCharsets.UTF_8);
         int sum = 0;
-        if (operation == "*") {
+        if (operation.equals("*")) {
             sum = 1;
         }
+        boolean firstLine = true;
 
         for ( String s : lines) {
+            if (firstLine) {
+                firstLine = false;
+                System.out.println(s);
+                sum += Integer.parseInt(s);
+                continue;
+            }
             switch (operation) {
                 case "+":
                     sum += Integer.parseInt(s);
-                    System.out.println(sum);
+                    System.out.println(String.format("+%s (=%d)", s, sum));
                     break;
                 case "*":
                     sum *= Integer.parseInt(s);
-                    System.out.println(sum);
+                    System.out.println(String.format("*%s (=%d)", s, sum));
                     break;
             }
+        }
+        if (operation.equals("+")) {
+            System.out.println(String.format("-------\ntotal = %s (addition)", sum));
+        } else {
+            System.out.println(String.format("-------\ntotal = %s (multiplication)", sum));
         }
     }
 }
